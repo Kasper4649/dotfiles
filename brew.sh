11a01ed2cba4570a0751f37b70e32cbe43cf41e4
Kasper4649 <imzxz123456@gmail.com>
2021-06-15 17:19:39 +0800

    add brew.sh

---
+++ brew.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+brew install tmux
+
+brew install neofetch
+brew install exa
+brew install fd
+brew install bat
+brew install httpie
+brew install tldr
+
+brew install youtube-dl
