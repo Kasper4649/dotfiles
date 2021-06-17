b1fc16decc821ac0b0bf72d04317d7f32d7b8cbd
Kasper4649 <imzxz123456@gmail.com>
2021-06-17 17:01:28 +0800

    modify brew.sh

--- brew.sh
+++ brew.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 brew install tmux
+brew install yadm
 
 brew install neofetch
 brew install exa
@@ -9,6 +10,5 @@ brew install bat
 brew install httpie
 brew install tldr
 brew install htop
-brew install yadm
 
 brew install youtube-dl
