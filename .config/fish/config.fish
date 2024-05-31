2751ccf3ab8a25c913f045d98e705bd8f6ed82f2
Kasper4649 <imzxz123456@gmail.com>
2024-05-31 23:42:14 +0800

    modify proxy

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -Ux http_proxy http://127.0.0.1:7890
     set -Ux https_proxy http://127.0.0.1:7890
-    set -Ux all_proxy socks://127.0.0.1:7890
+    set -Ux all_proxy socks5://127.0.0.1:7890
     set -x HOMEBREW_BOTTLE_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles"
     set -x HOMEBREW_API_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles/api"
 
